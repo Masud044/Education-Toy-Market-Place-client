aa3840ae4bdb9f529acd87968ba65711c8edef41
Masud <ranamasud0701@gmail.com>
2023-06-15 15:16:13 +0600

    added the shop category

--- src/Pages/Home/ShopCategory/ShopCategory.jsx
+++ src/Pages/Home/ShopCategory/ShopCategory.jsx
@@ -1,9 +1,61 @@
+import { useState } from "react";
+import useAllToy from "../../../Hook/useAllToy";
 
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import ToyCategory from "./ToyCategory";
 
 const ShopCategory = () => {
+
+    const [allToy] = useAllToy();
+
+    const MathToy = allToy.filter(item => item.subcategory === 'MathToy');
+    const LanguageToy = allToy.filter(item => item.subcategory === 'LanguageToy');
+    const EngineerToy = allToy.filter(item => item.subcategory === 'EngineerToy');
+
+    const [tabIndex, setTabIndex] = useState(0);
     return (
         <div>
             <h1 className="text-center font-bold text-fuchsia-800 text-3xl mb-8 mt-8">Shop Category</h1>
+
+            <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+                <TabList>
+                    <Tab>MathToy</Tab>
+                    <Tab>LanguageToy</Tab>
+                    <Tab>EngineerToy</Tab>
+                </TabList>
+                <TabPanel>
+                    <div className="md:flex justify-center items-center mt-4 gap-8">
+                    {
+                        MathToy.slice(0,2).map(item=> <ToyCategory key={item._id}
+                             item={item}
+                             ></ToyCategory>)
+                    }
+                    </div>
+                   
+                
+                </TabPanel>
+                <TabPanel>
+                <div className="md:flex justify-center items-center mt-4 gap-8">
+                    {
+                        LanguageToy.slice(0,2).map(item=> <ToyCategory key={item._id}
+                             item={item}
+                             ></ToyCategory>)
+                    }
+                    </div>
+                </TabPanel>
+                <TabPanel>
+                <div className="md:flex justify-center items-center mt-4 gap-8">
+                    {
+                        EngineerToy.slice(0,2).map(item=> <ToyCategory key={item._id}
+                             item={item}
+                             ></ToyCategory>)
+                    }
+                    </div>
+                </TabPanel>
+            </Tabs>
+
+
         </div>
     );
 };
---
+++ src/Pages/Home/ShopCategory/ToyCategory.jsx
@@ -0,0 +1,32 @@
+import { Rating } from '@smastrom/react-rating'
+
+import '@smastrom/react-rating/style.css'
+import { Link } from 'react-router-dom';
+
+
+const ToyCategory = ({item}) => {
+    const {image,toyName,price,rating,_id}=item
+    return (
+        <div className="card w-96 bg-fuchsia-300 p-4 shadow-xl">
+        <figure><img src={image} alt="Shoes" /></figure>
+        <div className="card-body">
+       
+          <h2 className="card-title text-2xl">{toyName}</h2>
+          <p>
+          <Rating
+                                style={{ maxWidth: 100 }}
+                                value={rating}
+                                readOnly
+                            />
+          </p>
+          <p className='font-semibold'>price: ${price}</p>
+          
+          <div className="card-actions justify-center">
+          <Link to={`/singletoy/${_id}`}> <button className="btn bg-fuchsia-800 text-white">View details </button></Link>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default ToyCategory;
